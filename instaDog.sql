f10f5d2c82da21d38bdb980f1a870f94ac6c0550
Claude Anthony <vous@exemle.com$>
2019-02-01 08:26:19 +0100

    modification finale

--- instaDog.sql
+++ instaDog.sql
@@ -11,25 +11,26 @@ CREATE TABLE Utilisateur (
     id INT(100) PRIMARY KEY AUTO_INCREMENT,
     utilisateur VARCHAR(100),
     motDePasse VARCHAR(255),
-    dateDernierConnexion DATE,
+    dateDernierConnexion DATE
 );
 
 
 CREATE TABLE Chien (
     id INT(100) PRIMARY KEY AUTO_INCREMENT,
     nomElevage VARCHAR(100),
-    nom VARCHAR(100),
+    surNom VARCHAR(100),
     dateNaissance DATE,
     sexe VARCHAR(100),
     race VARCHAR(100),
     user_id INT(100),
+    photo VARCHAR(100)
     FOREIGN KEY (user_id) REFERENCES Utilisateur(id)
 );
 
 
 CREATE TABLE Article (
     id INT(100) PRIMARY KEY AUTO_INCREMENT,
-    img VARCHAR(200),
+    photo VARCHAR(200),
     texte VARCHAR(250),
     dateDePublication DATE,
     id_chien INT(100),
